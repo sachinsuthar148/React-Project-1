eaa97ce0e23b517a5aee878bed06196d875851e4
2021pietcssachin148@poornima.org <2021pietcssachin148@poornima.org>
2023-08-24 14:20:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './About'
+import Home from './Home'
+import Services from './Services'
 
-function App() {
+
+
+export default function Nav() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+  <Routes>
+  {/* <Route path='/about' element={<div>Home12</div>}/> */}
+  <Route path='/' element={<Home/>}/>
+  <Route path='/services' element={<Services/>}/>
+  <Route path='/about' element={<About/>}/>
+</Routes>
+  
+  </BrowserRouter>
+  )
 }
-
-export default App;
